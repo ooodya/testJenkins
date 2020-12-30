4d05c61e7976fd3d5fea13a892ee95a74b77c112
Vadim <ooodya@gmail.com>
2020-12-30 12:49:20 +0300

    Initial commit

---
+++ src/test/java/com/zaycev/jenkins1/Jenkins1ApplicationTests.java
@@ -0,0 +1,15 @@
+package com.zaycev.jenkins1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jenkins1ApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
